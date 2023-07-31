e2faf69d24d951fa8221d994fbfde293e21cd3e3
Jean-François DEL NERO <jeanfrancoisdelnero@free.fr>
2023-07-31 20:06:19 +0200

    sat16 frame decoder.

--- sat16ctrl/src/sat16_frame.c
+++ sat16ctrl/src/sat16_frame.c
@@ -37,3 +37,68 @@ int encode_sat16_frame(sat16ctrl_ctx * ctx,unsigned char address, unsigned char 
 
 	return i;
 }
+
+int decode_sat16_frame(sat16ctrl_ctx * ctx, sat_16_rx_frame_state * frame_state, unsigned char rx_byte )
+{
+	int ret;
+	unsigned short crc_value;
+
+	ret = 0;
+
+	switch(frame_state->frame_size)
+	{
+		case 0:
+			// Wait Start byte
+			if(rx_byte == 0xAA)
+			{
+				frame_state->full_frame[frame_state->frame_size++] = rx_byte;
+			}
+		break;
+		case 1:
+			// Address
+			frame_state->full_frame[frame_state->frame_size++] = rx_byte;
+			frame_state->address = rx_byte;
+		break;
+		case 2:
+			// Size
+			frame_state->full_frame[frame_state->frame_size++] = rx_byte;
+			frame_state->data_size = rx_byte;
+		break;
+		default:
+			if( frame_state->frame_size - 3 < frame_state->data_size ) // Data
+			{
+				frame_state->data[frame_state->frame_size - 3] = rx_byte;
+				frame_state->full_frame[frame_state->frame_size++] = rx_byte;
+			}
+			else
+			{
+				switch((frame_state->frame_size - frame_state->data_size) - 3)
+				{
+					case 0: //CRC L
+						frame_state->crc = rx_byte;
+						frame_state->full_frame[frame_state->frame_size++] = rx_byte;
+					break;
+					case 1: //CRC H
+						frame_state->crc |= (rx_byte<<8);
+						frame_state->full_frame[frame_state->frame_size++] = rx_byte;
+					break;
+					case 2: //EOF - 0x04
+						crc_value = crc16_buf(frame_state->full_frame, frame_state->frame_size, 0x0000);
+
+						frame_state->full_frame[frame_state->frame_size++] = rx_byte;
+
+						if( !crc_value && rx_byte == 0x04)
+							ret = 1;
+						else
+							ret = -2;
+					break;
+					default:
+						ret = -1;
+					break;
+				}
+			}
+		break;
+	}
+
+	return ret;
+}
--- sat16ctrl/src/sat16_frame.h
+++ sat16ctrl/src/sat16_frame.h
@@ -5,4 +5,17 @@
 // Written by: Jean-François DEL NERO
 ///////////////////////////////////////////////////////////////////////////////////
 
+typedef struct sat_16_rx_frame_state_
+{
+	unsigned char  full_frame[3 + 256 + 3];
+	int            frame_size;
+
+	unsigned char  address;
+	unsigned char  data[256];
+	unsigned int   data_size;
+	unsigned short crc;
+}sat_16_rx_frame_state;
+
 int encode_sat16_frame(sat16ctrl_ctx * ctx,unsigned char address, unsigned char * data, int data_size, unsigned char * out_frame);
+
+int decode_sat16_frame(sat16ctrl_ctx * ctx, sat_16_rx_frame_state * frame_state, unsigned char rx_byte );
